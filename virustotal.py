6d0a353a218aaee8ff870b2c081ea1cbcea0490b
Drewshg312 <10493450+drew1kun@users.noreply.github.com>
2017-12-02 13:42:55 -0800

    creating html table - work in progress

--- virustotal.py
+++ virustotal.py
@@ -6,6 +6,8 @@ import json
 import sys
 import time
 
+# import webserver
+
 API_KEY = '2a11b9bed44b9580bde1033624b38d32fad0c470a8611dc5928ee8d85060745a'
 
 virustotal = VirusTotalPublicApi(API_KEY)
@@ -19,20 +21,26 @@ for line in lines:
     response = virustotal.get_file_report(line)
     # Convert json to dictionary:
     json_data = json.loads(json.dumps(response))
+
+    html = """<html><table border="1">
+    <tr><th>hash_value (MD5)</th><th>FORTINET detection names </th><th>Number of engines detected</th><th>Scan Date</th></tr>"""
+
     if json_data['results']['response_code'] == 1 and \
        'Fortinet' in json_data['results']['scans']:
-        print(json_data['results']['md5'],
-            "|",
-            json_data['results']['scans']['Fortinet']['result'],
-            "|",
-            json_data['results']['positives'],
-            "|",
-            json_data['results']['scan_date'],
-            "|")
+            html += "<tr><td>{}</td>".format(json_data['results']['md5'])
+            html += "<tr><td>{}</td>".format(json_data['results']['scans']['Fortinet']['result'])
+            html += "<tr><td>{}</td>".format(json_data['results']['positives'])
+            html += "<tr><td>{}</td>".format(json_data['results']['scan_date'])
+
     elif json_data['results']['response_code'] == 1 and \
-         'Fortinet' not in json_data['results']['scans']:
-        print(line, "|", "hash is not known to Fortinet", "|")
+            'Fortinet' not in json_data['results']['scans']:
+            html += "<tr><td>{}</td>".format(json_data['results']['md5'])
+            html += "<tr><td>{}</td>".format('Not known to Fortinet')
     else:
-        print(line, "|", "hash is not in virustotal database", "|")
+            html += "<tr><td>{}</td>".format(json_data['results']['md5'])
+            html += "<tr><td>{}</td>".format('Hash is not in database')
+
+    html += "</tr>"
+    html += "</table></html>"
 
 f.close()
